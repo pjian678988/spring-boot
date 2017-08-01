0c7e4f7883af9f3a02975fb5a697e9724515a06c
peter <pjian678988@gmail.com>
2017-08-01 17:06:34 +0800

    add test

--- src/main/java/cn/xplanet/controller/SampleController.java
+++ src/main/java/cn/xplanet/controller/SampleController.java
@@ -1,22 +1,19 @@
 package cn.xplanet.controller;
 
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
 
-	@RequestMapping("/")
+	@RequestMapping("/test")
 	@ResponseBody
 	String home() {
 		return "Hello World!";
 	}
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SampleController.class, args);
-	}
 }
